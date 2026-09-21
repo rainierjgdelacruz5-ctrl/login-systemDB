84a4e6a023112f1cddd6d8546a94be5c4bd4601d
rainierjgdelacruz5-ctrl <rainierjgdelacruz5@gmail.com>
2026-09-21 22:02:45 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -118,13 +118,14 @@ app.get("/api/profile", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("MongoDB connected.");
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
   })
   .catch((error) => {
     console.error("MongoDB connection failed:", error);
